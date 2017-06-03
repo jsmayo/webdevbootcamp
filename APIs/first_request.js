69876588c271c0521a8d9d217fed9dffacd8c779
Steven <jsmayo91@gmail.com>
2017-06-03 22:41:17 +0000

    Updates documentation

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-
+//yahoo weather api
 request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function (error, response, body) {
     //%20 is how you encode a space
     if(!error && response.statusCode == 200) {
