c8d66910405779ce869f7c7ed7601aded5d42da8
Steven <jsmayo91@gmail.com>
2017-06-07 07:39:23 +0000

    Adds: Mongo support for campground storage   1. User can now input a new campground and it will be 	store as a db entry within mongoDB.   2. Old code was commented out and left for demonstration purposes.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,16 +1,35 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//create the Schema/Pattern
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
 
+//compile the model 
+var Campground = mongoose.model("Campground", campgroundSchema);
 
- var campgrounds = [
-       {name: "Seagull", image: "http://domokur.com/display/images/002043_Seagull--Seafarer-15.jpg"},
-       {name: "Don Lee", image: "https://nccumc.org/files/oh.jpg"},
-       {name: "Seafarer", image: "https://s-media-cache-ak0.pinimg.com/originals/84/c1/10/84c110dc06841c3364fc6a1e3f596ecb.jpg"}
-       ];
+// Campground.create(
+//     {name: "Don Lee", 
+//     image: "https://nccumc.org/files/oh.jpg"}, 
+//     function(err, campground) {
+//         if(err) console.log(err);
+//         else console.log("new campground " + campground);
+    
+//     });
+
+//  var campgrounds = [
+//       {name: "Seagull", image: "http://domokur.com/display/images/002043_Seagull--Seafarer-15.jpg"},
+//       {name: "Don Lee", image: "https://nccumc.org/files/oh.jpg"},
+//       {name: "Seafarer", image: "https://s-media-cache-ak0.pinimg.com/originals/84/c1/10/84c110dc06841c3364fc6a1e3f596ecb.jpg"}
+//       ];
        
       
 app.get("/", function(req, res) {
@@ -19,9 +38,11 @@ app.get("/", function(req, res) {
 
 
 app.get("/campgrounds", function(req, res) {
-  
-       
-       res.render("campgrounds", {campgrounds: campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds) {
+        if(err) console.log(err);
+        else res.render("campgrounds", {campgrounds: allCampgrounds});
+    })
 })
 
 app.get("/campgrounds/new", function(req, res) {
@@ -34,9 +55,11 @@ app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
+   Campground.create(newCampground, function(err, newlyCreated) {
+       if(err) console.log(err)
+       else  res.redirect("/campgrounds"); //default redirect is a GET request (basically refresh)  
+   })
     // redirect back to campgrounds page
-    res.redirect("/campgrounds"); //default redirect is a GET request (basically refresh)
 })
 
 
