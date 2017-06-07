58c653a5fed6638e43df351cef4e8771f7b4a430
Steven <jsmayo91@gmail.com>
2017-06-07 20:33:22 +0000

    Adds: Skeleton code for blog app with RESTful Route Definitions.
      1. Skeleton code not yet complete.
      2. Package file created for dependencies.

---
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -0,0 +1,47 @@
+var bodyParser      = require("body-parser"),
+mongoose            = require("mongoose"),
+express             = require("express"),
+app                 = express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/restful-blog-app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    // If I wanted a default image {type: String, default: "placeholderimage.jpg"}
+    body: String,
+    //Type is Date, but I dont want the user to type in the date.
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+/*
+Used to test initial server start-up
+    Blog.create({
+        title: "Test Blog",
+        image: "https://tse4.mm.bing.net/th?id=OIP.6va4y4_qipIBDIlzgb8rkQE8DF&w=257&h=160&c=7&qlt=90&o=4&pid=1.7",
+        body: "This is a test"
+        }, function(err, post) {
+            if(err) console.log(err);
+            else console.log("post successful");
+        })
+*/
+
+// ROUTES
+app.get("/blogs", function(req, res) {
+    res.render("index");
+});
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server is running");
+});
