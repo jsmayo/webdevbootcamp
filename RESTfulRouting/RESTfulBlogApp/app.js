f4b97281f5d1d79cb738bdb05921143859aadcb4
Steven <jsmayo91@gmail.com>
2017-06-10 13:59:17 +0000

    Adds: Delete, Show, and Edit routes   1. Adds dependency on method-override for PUTS   2. Updates styling, but still in progress.

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -1,4 +1,5 @@
 var bodyParser      = require("body-parser"),
+methodOverride      = require("method-override"),
 mongoose            = require("mongoose"),
 express             = require("express"),
 app                 = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/restful-blog-app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -56,7 +58,7 @@ app.get("/blogs/new", function(req, res) {
 
 // CREATE ROUTE
 app.post("/blogs", function(req, res) {
-    Blog.create(req.body.blog, function(err, newBlow) {
+    Blog.create(req.body.blog, function(err, newBlog) {
         if(err) res.render("new");
         else res.redirect("/blogs");
             
@@ -74,6 +76,33 @@ app.get("/blogs/:id", function(req, res) {
     });
 });
 
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+   Blog.findById(req.params.id, function(err, foundBlog) {
+       if(err) res.redirect("/blogs");
+       else(res.render("edit", {blog: foundBlog}));
+   });
+});
+
+// UPDATE ROUTE:
+
+app.put("/blogs/:id", function(req, res) {
+    // findByIdAndUpdate requires 3 parameters: locatorID, newData, & callback
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if(err) res.redirect("/blogs");
+        //redirect to newly edited post, which will have updated info
+        else res.redirect("/blogs/" + req.params.id);
+    });
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if(err) res.redirect("/blogs");
+        else res.redirect("/blogs");
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server is running");
