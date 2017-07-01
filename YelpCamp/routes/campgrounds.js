f653ad0f5f24973749c230d7eb5503d223e4b94c
Steven <jsmayo91@gmail.com>
2017-07-01 22:34:25 +0000

    Adds: User association to campground creation

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -15,17 +15,20 @@ router.get("/", function(req, res) {
     });
 });
 
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
     res.render("campgrounds/new");
 });
 
 
-router.post("/", function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
    // get data form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username };
+    var newCampground = {name: name, image: image, description: desc, author: author};
     
     //use the above variables to create an object and add it to the DB.
    Campground.create(newCampground, function(err, newlyCreated) {
@@ -46,7 +49,7 @@ router.post("/", function(req, res) {
     - Had to delete ALL campgrounds when I added the new description property.
     - db.collection.drop() : removes all data from the collection
 */
-router.get("/:id", function(req, res) {
+router.get("/:id", isLoggedIn, function(req, res) {
     //Find the campground with the provided ID.
     Campground.findById(req.params.id).populate("comments").exec( function(err, foundCampground) {
         if(err) console.log(err);
@@ -54,5 +57,16 @@ router.get("/:id", function(req, res) {
     });
     //Show the information about the ID
 });
+
+//Middleware
+/*Make sure a user cannot comment if not logged into the site:
+  - you can use this anywhere, but I'm placing into comment route*/
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next(); //run the next function defined after the middleware
+    }
+     // make the user login if not authenticated
+    res.redirect("/login");
+}
 
 module.exports = router;
