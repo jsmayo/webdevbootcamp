3847739fde329d6d2d4e3c48831301a750f30493
Steven <jsmayo91@gmail.com>
2017-07-06 23:58:37 +0000

    Initial commit

---
+++ DeployingDemo/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+app.listen(process.env.IP, process.env.PORT);
